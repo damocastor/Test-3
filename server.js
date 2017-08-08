bc455df1a0464bdd54479de2cc78fac82adbeb8c
David Castor <damocastor@gmail.com>
2017-08-08 14:37:01 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const hbs = require('hbs')
+const pug = require('pug')
+const fs = require('fs')
+
+var app = express(); 
+
+app.set('view engine', 'pug')
+
+app.use((req, res, next) => {
+	const now = new Date().toString()
+	const log = `${now}: ${req.method} ${req.url}`
+
+	console.log(log)
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server.log')
+		}
+	})
+	next()
+})
+
+// app.use((req, res, next) => {
+// 	res.render('404.pug', {
+// 		pageTitle: 'Maintainence'
+// 	})
+// })
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+ res.render('home.pug', {
+	 pageTitle: 'Home Page'
+ })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.pug', {
+			pageTitle: 'About Page'
+		})
+})
+
+app.get('/bad', (req,res) => {
+  res.send(
+    {
+    	errorMessage: 'Unable to handle request'
+    }
+	)
+})
+
+app.listen(8080, () => {
+	console.log("Server running on port 8080")
+})
